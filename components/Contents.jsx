d4fbac061491cc91b2c948ffaa58cd1cc90d369a
VaishnavAkash <akashgupta9062@gmail.com>
2024-05-01 15:35:57 +0530

    update : fixed bugs

--- components/Contents.jsx
+++ components/Contents.jsx
@@ -39,10 +39,15 @@ const Contents = () => {
     }
 
 
-    const handleFavourites = async (rank) => {
+    const handleFavourites = async (event, rank) => {
+        event.stopPropagation();
         const data = await addToFav(rank);
         console.log(data);
         handleToast(data.message);
+    }
+
+    const handleOpenMovie = (rank) => {
+        Router.push(`/movie?rank=${rank}`);
     }
 
     useEffect(() => {
@@ -75,17 +80,17 @@ const Contents = () => {
                 </div>
                 <div className='flex h-full gap-8 overflow-x-scroll'>
                     {movies ? movies?.map(item => {
-                        return <Link key={uuidv4()} href={`/movie?rank=${item?.rank}`} className='relative'>
+                        return <div onClick={handleOpenMovie} className='relative'>
                             <div className='absolute top-0 left-0 w-full h-full gradient-bg rounded-lg'></div>
                             <div className='group min-w-[12rem] relative gradient-overlay cursor-pointer'>
                                 <img src={item?.image} className='w-full' alt='movie img' />
-                                <CiHeart onClick={() => { handleFavourites(item?.rank) }} className='absolute top-4 right-4 hidden group-hover:block text-white font-extrabold cursor-pointer  text-3xl' />
+                                <CiHeart onClick={(e) => { handleFavourites(e, item?.rank) }} className='absolute top-4 right-4 hidden group-hover:block text-white font-extrabold cursor-pointer  text-3xl' />
                                 <div className='absolute w-full bottom-2 text-center text-white'>
                                     <h2 className='font-semibold text-sm'>{item?.title}</h2>
                                     <p className='font-medium text-xs'>{item?.genre.join(', ')}</p>
                                 </div>
                             </div>
-                        </Link>
+                        </div>
                     }) : <Loader />}
                 </div>
             </div>
